b78be95eea9400dec755943de660b6f3714ef9b6
sahlct <mhdsahlct@gmail.com>
2025-03-28 17:23:49 +0530

    hy

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
-const express = require('express');
-require("dotenv").config();
-const { Pool } = require('@neondatabase/serverless');
+const express = require('express')
 
-const app = express();
-app.use(express.json());
+const port = 3000
+const app = express()
+app.use(express.json())
 
-// Use environment variable for the database connection string
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+app.use('/categories', require('./routes/categoryRoutes'))
+app.use('/products', require('./routes/productsRoutes'))
 
-app.get('/', async (req, res) => {
-  try {
-    // Execute a sample query
-    const { rows } = await pool.query('SELECT version()');
-    const version = rows[0].version;
-    res.status(200).send(`PostgreSQL Version: ${version}`);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-app.use('/categories', require('./routes/categoryRoutes'));
-app.use('/products', require('./routes/productsRoutes'));
+app.listen(port, ()=>console.log(`Server Started on Port ${port}`))
 
 module.exports = app;
