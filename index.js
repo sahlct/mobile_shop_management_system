0b8f587588d25eebb56e5847eec1d261d0885120
sahlct <mhdsahlct@gmail.com>
2025-03-28 16:39:34 +0530

    latest

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const port = 3000
 const app = express()
 app.use(express.json())
 
+require("dotenv").config();
+
+const http = require("http");
+const { neon } = require("@neondatabase/serverless");
+
+const sql = neon(process.env.DATABASE_URL);
+
+const requestHandler = async (req, res) => {
+  const result = await sql`SELECT version()`;
+  const { version } = result[0];
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(version);
+};
+
+http.createServer(requestHandler).listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
+
 app.use('/categories', require('./routes/categoryRoutes'))
 app.use('/products', require('./routes/productsRoutes'))
 
