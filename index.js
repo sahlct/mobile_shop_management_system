2a18dfffdb2c0c5c2d4bd16156e68c66f70675ce
prosevodeveloper <prosevotechnologies@gmail.com>
2025-07-02 20:44:16 +0530

    updated

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express = require('express')
+const cors = require('cors');
 
 const port = 5000;
 const app = express()
 app.use(express.json())
 
+
 app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
+    origin: 'http://localhost:3000',  // your local frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
 }));
+
+app.options('*', cors()); // Handle preflight requests
 
 app.use('/users', require('./routes/userRoutes'))
 app.use('/mobiles', require('./routes/mobilesRoutes'))
 app.use('/accessories', require('./routes/accessoriesRoutes'))
 app.use('/services', require('./routes/serviceRoutes'))
 
-app.listen(port, ()=>console.log(`Server Started on Port ${port}`))
+app.listen(port, () => console.log(`Server Started on Port ${port}`))
 
 // module.exports = app;
